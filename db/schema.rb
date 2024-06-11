4bb13016c566c1d3e43e34af334de1311fac5e24
Andrew Canfield <Andrew@AndrewCanfield.com>
2024-06-11 14:51:33 -0700

    Added owner who can have many pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_09_173229) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_023511) do
+  create_table "owners", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "kind"
@@ -18,6 +24,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_09_173229) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "weight", precision: 5, scale: 2
+    t.integer "owner_id", null: false
+    t.index ["owner_id"], name: "index_pets_on_owner_id"
   end
 
+  add_foreign_key "pets", "owners"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,26 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Clear existing data
+
 Pet.destroy_all
+Owner.destroy_all
+
+Owner.create([
+    {name: "Arbuckle"},
+    {name: "Granny"}
+])
+
+puts "Created #{Owner.count} Owners"
 
 # Create pets
-Pet.create!(
-  [
-    { name: 'Buddy', kind: 'Dog', breed: 'Golden Retriever', weight: 60 },
-    { name: 'Whiskers', kind: 'Cat', breed: 'Siamese', weight: 8.5 },
-    { name: 'Tweety', kind: 'Bird', breed: 'Canary', weight: 1 },
-    { name: 'Nemo', kind: 'Fish', breed: 'Clownfish', weight: 0.25 }
-  ]
-)
+Owner.find_by(name: 'Arbuckle').pets.create!([
+  { name: 'Buddy', kind: 'Dog', breed: 'Golden Retriever', weight: 60 },
+  { name: 'Whiskers', kind: 'Cat', breed: 'Siamese', weight: 8.5 },
+])
+
+Owner.find_by(name: 'Granny').pets.create!([
+  { name: 'Tweety', kind: 'Bird', breed: 'Canary', weight: 1 },
+  { name: 'Nemo', kind: 'Fish', breed: 'Clownfish', weight: 0.25}
+])
 
 puts "Created #{Pet.count} pets"
